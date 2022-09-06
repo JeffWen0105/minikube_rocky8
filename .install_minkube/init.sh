0b963cbff0829bd65f8dd86b9a3f9250ed5539ae
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2022-09-06 15:07:28 +0800

    Update init.sh

--- .install_minkube/init.sh
+++ .install_minkube/init.sh
@@ -7,7 +7,7 @@ function init(){
   sudo dnf -y copr enable rhcontainerbot/container-selinux
   sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_8/devel:kubic:libcontainers:stable.repo
   sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:${VERSION}/CentOS_8/devel:kubic:libcontainers:stable:cri-o:${VERSION}.repo
-  echo "tgc101" | passwd --stdin root
+  echo "tgc101" | sudo passwd --stdin root
 }
 
 log_info "Init ...."
